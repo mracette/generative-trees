311215d289fc2d9af9916290bbb476c3f1635a3e
Mark Racette <markracette@gmail.com>
2019-05-23 10:55:41 -0400

    put tree in its own class

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 // inspired by the @mrdoob sketch: https://threejs.org/editor/#file=https://mrdoob.neocities.org/030/app.json
 const THREE = window.THREE = require('three');
-const FirstPersonControls = require('./controls/first_person_controls.js');
 const OrbitControls = require('./controls/orbit_controls.js');
+const AppleTree = require('./apple_tree.js');
 const canvas = document.getElementById("canvas");
 
 let camera, scene, renderer, camControls;
-let treeGroup = new THREE.Group();
-let leafGroup = new THREE.Group();
+let subjects = {};
 let clock = new THREE.Clock();
-
-let leafGeo = new THREE.SphereGeometry(0.5,8,8);
-let leafMat = new THREE.MeshBasicMaterial({color: 0x0000ff});
-let leaf = new THREE.Mesh(leafGeo, leafMat);
 
 init();
 bindEventListeners();
@@ -46,102 +41,14 @@ function init(){
 	const farPlane = 10000; 
 	camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
 	camera.position.set(0,0,50);
-	//camera.lookAt(new THREE.Vector3(0,5,0));
+	camera.lookAt(new THREE.Vector3(0,5,0));
 
 	//controls
 	camControls = new THREE.OrbitControls(camera);
 	camControls.autoRotate = true;
 
 	//make a tree
-	generate(7);
-
-}
-
-function generate(maxDepth) {
-
-	let depth = 1;
-
-	let rootGeo = new THREE.BoxGeometry(1,1,1);
-	let rootMat = new THREE.MeshBasicMaterial({color: 0x000000});
-	let root = new THREE.Mesh(rootGeo, rootMat);
-	root.scale.set(1,5,1);
-
-	treeGroup.add(root);
-	addLayer(root, depth, maxDepth);
-	scene.add(treeGroup);
-	scene.add(leafGroup);
-
-}
-
-function addLayer(child, depth, maxDepth){
-
-	if (depth === maxDepth) {return};
-
-	let widthScale = 0.6;
-	let heightScale = Math.pow(0.95,depth);
-
-	let nBranches = (maxDepth-depth) - Math.floor(1.5*depth*Math.random());
-	if(depth === maxDepth - 1) {nBranches = 0;}
-
-	if(nBranches > 0) {
-		for(let i = 0; i < nBranches; i++){
-
-			let branch = child.clone();
-
-			// set new branch size
-			branch.scale.set(
-				child.scale.x * widthScale, 
-				child.scale.y * heightScale, 
-				child.scale.z * widthScale
-			);
-
-			// move branch up
-			branch.translateY(child.scale.y * (heightScale));
-
-			// rotate branch
-			let rotation = {
-				x: Math.random() - 0.5,
-				y: Math.random() - 0.5,
-				z: Math.random() - 0.5
-			};
-
-			// rotate on z axis and adjust corresponding x offset
-			branch.rotation.z += rotation.z;
-			branch.translateX(-0.5 * child.scale.y * heightScale * Math.sin(rotation.z));
-			branch.translateY(0.5 * branch.scale.y * heightScale * (Math.cos(rotation.z) - 1));
-
-			// rotate on x axis and adjust corresponding z offsets
-			branch.rotation.x += rotation.x;
-			branch.translateZ(0.5 * child.scale.y * heightScale * Math.sin(rotation.x));
-			branch.translateY(0.5 * child.scale.y * heightScale * (Math.cos(rotation.x) - 1));
-
-			treeGroup.add(branch);
-
-			console.log(new THREE.Vector3(rotation.x, rotation.y, rotation.z).normalize());
-
-			addLayer(branch, depth+1, maxDepth);
-		}
-	} else {
-		if(1 > Math.random()){
-			let newLeaf = leaf.clone();
-
-			//copy position of the last branch
-			newLeaf.position.copy(child.position);
-
-			//move to the tip of the branch (based on x-rotation)
-			newLeaf.translateY(0.5 * child.scale.y * (Math.cos(child.rotation.x)));
-			newLeaf.translateZ(0.5 * child.scale.y  * Math.sin(child.rotation.x))
-
-			//move to the tip of the branch (based on z-rotation)
-			newLeaf.translateY(0.5 * child.scale.y * (Math.cos(child.rotation.z) - 1));
-			newLeaf.translateX(-0.5 * child.scale.y  * Math.sin(child.rotation.z));
-
-			leafGroup.add(newLeaf);
-			return;
-		} else {
-			return;
-		}
-	}
+	subjects.appleTree = new AppleTree(scene, 7);
 
 }
 
@@ -151,12 +58,10 @@ function animate() {
 }
 
 function render() {
-	camControls.update(clock.getDelta());
-	let t = 0.8;
-	leafMat.color.setRGB(
-		Math.cos(clock.getElapsedTime()*t),
-		Math.tan(clock.getElapsedTime()*t),
-		Math.sin(clock.getElapsedTime()*t));
+	let d = clock.getDelta();
+	let e = clock.getElapsedTime();
+	camControls.update(d);
+	subjects.appleTree.update(d, e);
     renderer.render(scene, camera);
 }
 
