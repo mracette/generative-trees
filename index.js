2e28cd6e37531d22c15c45f9c944332486406bce
Mark Racette <markracette@gmail.com>
2019-05-30 09:32:50 -0400

    add import/export ability

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // inspired by the @mrdoob sketch: https://threejs.org/editor/#file=https://mrdoob.neocities.org/030/app.json
 const THREE = window.THREE = require('three');
 const OrbitControls = require('./controls/orbit_controls.js');
+const GLTFExporter = require('./gltf/GLTFExporter.js');
+const GLTFLoader = require('./gltf/GLTFLoader.js');
 const AppleTree = require('./apple_tree.js');
-const canvas = document.getElementById("canvas");
 
-let camera, scene, renderer, camControls;
+let camera, scene, renderer, camControls, exporter;
+let currentModel;
 let subjects = {};
 let clock = new THREE.Clock();
+
+const canvas = document.getElementById("canvas");
+const dlButton = document.getElementById("dl");
 
 init();
 bindEventListeners();
@@ -24,7 +29,8 @@ function init(){
 
 	//scene
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color('#1F262F');
+	//scene.background = new THREE.Color('#1F262F');
+	scene.background = new THREE.Color('#FFFFFF');
 
 	//renderer
 	renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true, alpha: true}); 
@@ -47,8 +53,12 @@ function init(){
 	camControls = new THREE.OrbitControls(camera);
 	camControls.autoRotate = true;
 
+	//exporter
+	exporter = new THREE.GLTFExporter();
+
 	//make a tree
 	subjects.appleTree = new AppleTree(scene, 7);
+	//exportModel(subjects.appleTree.treeGroup, 'apple-tree');
 
 }
 
@@ -70,3 +80,20 @@ function onWindowResize(newWidth, newHeight){
     camera.updateProjectionMatrix();
     renderer.setSize(newWidth, newHeight);
 }
+
+function exportModel(subject, fileName){
+	exporter.parse(subject, (obj) => {
+		downloadObjectAsJson(obj, fileName);
+		console.log('Exported the following object: ' + obj);
+	});
+}
+
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
