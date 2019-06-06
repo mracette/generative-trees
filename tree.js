06eb86d54b189fd607f996a63aae2e57a13c6b5d
Mark Racette <markracette@gmail.com>
2019-06-06 16:55:37 -0400

    shader material for leaves

--- tree.js
+++ tree.js
@@ -6,6 +6,14 @@ class Tree {
 
         this.treeGroup = new THREE.Group();
 
+        console.log(typeof this.getFragmentShader());
+
+        this.uniforms = {
+            u_time: {type: 'f', value: 1.0},
+            u_resolution: {type: 'v2', value: new THREE.Vector2()},
+            u_mouse: {type: 'v2', value: new THREE.Vector2()}
+        };
+
         this.branchGeos = [];
         this.leafGeos = [];
 
@@ -14,8 +22,17 @@ class Tree {
         this.branch = new THREE.Mesh(this.branchGeo, this.branchMat);
 
         this.leafGeo = new THREE.SphereBufferGeometry(0.5,8,8);
-        this.leafMat = new THREE.MeshBasicMaterial({color: 0x0000ff});
-        this.leaf = new THREE.Mesh(this.leafGeo, this.leafMat);
+        this.leafBasicMat = new THREE.MeshBasicMaterial({color: 0x0000ff});
+        this.leaf = new THREE.Mesh(this.leafGeo, this.leafBasicMat);
+
+        let vertexShader = this.getVertexShader();
+        let fragmentShader = this.getFragmentShader();
+
+        this.leafShaderMat = new THREE.RawShaderMaterial({
+            uniforms: this.uniforms,
+            fragmentShader: fragmentShader,
+            vertexShader: vertexShader
+        })
 
         this.generate(scene, depth);
     }
@@ -37,12 +54,28 @@ class Tree {
         // merge buffer geos
         let branchGeo = THREE.BufferGeometryUtils.mergeBufferGeometries(this.branchGeos);
         let leafGeo = THREE.BufferGeometryUtils.mergeBufferGeometries(this.leafGeos);
-  
+
         // merge vertices
         branchGeo = THREE.BufferGeometryUtils.mergeVertices(branchGeo, .001);
 
-        let leaves = new THREE.Mesh(leafGeo, this.leafMat);
+        // add color attribute
+        let colors = [];
+
+        for(let i = 0; i < this.leafGeos.length; i++){
+            let r = Math.random();
+            let g = Math.random();
+            let b = Math.random();
+            for(let j = 0; j < this.leafGeo.getAttribute('position').count * 3; j++){
+                colors.push(r, g, b, 1);
+            }
+        }
+
+        leafGeo.addAttribute('color', new THREE.Float32BufferAttribute(new Float32Array(colors), 4));
+        console.log(leafGeo);
+
+        let leaves = new THREE.Mesh(leafGeo, this.leafShaderMat);
         let branches = new THREE.Mesh(branchGeo, this.branchMat);
+
         let tree = new THREE.Group().add(leaves, branches);
         console.log(tree);
         scene.add(tree);
@@ -123,13 +156,55 @@ class Tree {
         }
     }
 
-    update(d, e) {
-        let t = 0.8;
-        this.leafMat.color.setRGB(
-            Math.cos(e*t),
-            Math.tan(e*t),
-            Math.sin(e*t)
-        );
+    update(d, e){
+        this.uniforms.u_time.value = e;
+    }
+
+    getVertexShader(){
+        return `
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+
+        uniform mat4 modelViewMatrix;
+        uniform mat4 projectionMatrix;
+        uniform vec2 u_resolution;
+
+        attribute vec3 position;
+        attribute vec4 color;
+
+        varying vec4 vColor;
+
+        void main() {
+            vColor = color;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+        `;
+    }
+
+    getFragmentShader(){
+        return `
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+
+        uniform vec2 u_resolution;
+        uniform float u_time;
+
+        varying vec4 vColor;
+
+        void main() {
+            vec2 st = gl_FragCoord.xy / u_resolution.xy;
+            gl_FragColor = vColor;
+            float glow = 0.2;
+            gl_FragColor = vec4(
+                mix(vColor.x, 1., glow * sin(u_time*1.)),
+                mix(vColor.y, 1., glow * cos(u_time*2.)),
+                mix(vColor.z, 1., glow * sin(u_time*5.)),
+                1.
+             );
+        }
+        `;
     }
 
 }
